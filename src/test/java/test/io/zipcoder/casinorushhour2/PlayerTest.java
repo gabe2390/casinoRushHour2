682eb02e355fa036b81b45b04dd455edd62f03b2
Richard Sparks <rsparks@Zips-MacBook-Pro-43.local>
2015-09-23 15:29:37 -0400

    Player class finished

--- src/test/java/test/io/zipcoder/casinorushhour2/PlayerTest.java
+++ src/test/java/test/io/zipcoder/casinorushhour2/PlayerTest.java
@@ -12,16 +12,20 @@ public class PlayerTest {
 
     Player player1 = new Player ("Rick");
 
-    //Tests getName
-    @Test
 
+    /**
+     * tests getName() gets the name of the player
+     */
+    @Test
     public void testGetName(){
         player1 = new Player("Rick");
         assertEquals("Should return whatever is in the name field","Rick",player1.getName());
     }
 
 
-    //Tests bet
+    /**
+     * Tests bet(), places bet and updates bank
+     */
     @Test
 
     public void testBet(){
@@ -29,7 +33,9 @@ public class PlayerTest {
         assertEquals("Should return bet value", 2500, player1.bet(2500));
     }
 
-    //Tests getBank
+    /**
+     * Tests getBank(), gets bank total
+     */
     @Test
 
     public void testGetBank(){
