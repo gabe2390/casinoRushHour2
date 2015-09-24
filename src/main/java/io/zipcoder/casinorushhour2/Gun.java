31e00a782f9aef0afd8db263b503b2cb2cde2fc0
Gabriel Humphrey <ghumphrey@Zips-MacBook-Pro-3.local>
2015-09-24 08:12:29 -0400

    Russian Roulette and Gun updated

--- src/main/java/io/zipcoder/casinorushhour2/Gun.java
+++ src/main/java/io/zipcoder/casinorushhour2/Gun.java
@@ -7,29 +7,56 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by ghumphrey on 9/23/15.
+ * Created by Gabriel Humphrey on 9/23/15.
+ * This is the Gun class that will be used for every game of Russian Roulette and any other game requiring a gun
  */
 public class Gun {
     private Player player;
-    private boolean[] bulletChambers;
+    private ArrayList<Boolean> bulletChambers;
 
+    /**
+     * Constructor takes a Player object and sets it the player field. It also adds 6 Boolean values to the
+     * bulletChamber List, one true, 5 false with the true value representing the single bullet
+     * @param player
+     */
     public Gun(Player player) {
         this.player = player;
-        bulletChambers = new boolean[]{true,false,false,false,false,false};
+        bulletChambers = new ArrayList<Boolean>();
+
+        for (int i = 0; i < 6; i++) {
+            if (i == 0) bulletChambers.add(true);
+            else bulletChambers.add(false);
+        }
     }
-    public boolean[] getBulletChamber(){
+
+    /**
+     *
+     * @return ArrayList<Boolean> - returns the bulletChambers List
+     */
+    public ArrayList<Boolean> getBulletChambers() {
         return bulletChambers;
     }
+
+    /**
+     * shuffles the values in the bulletChambers List
+     */
     public void spinChamber() {
-        Collections.shuffle(Arrays.asList(bulletChambers));
+        Collections.shuffle(bulletChambers);
     }
 
+    /**
+     *
+     * @return returns the value of the first index in the bulletChambers List
+     */
     public Boolean shoot() {
-        return null;
+        return bulletChambers.get(0);
     }
 
-
+    /**
+     *
+     * @return returns the player object that was set during construction of the Gun
+     */
     public Player getPlayer() {
-        return null;
+        return player;
     }
 }
