84912ac48d63bc08437e216e8f9595ff0bfc8f95
Richard Sparks <rsparks@Zips-MacBook-Pro-43.local>
2015-09-24 09:41:24 -0400

    Commented CardGame

--- src/main/java/io/zipcoder/casinorushhour2/CardGame.java
+++ src/main/java/io/zipcoder/casinorushhour2/CardGame.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 /**
  * Created by rsparks on 9/22/15.
+ * Adds a dealer object to a CardGame type
  */
 
-public interface CardGame extends Game{
-Dealer DEALER = new Dealer();
+public interface CardGame extends Game {
+
+    /**
+     *Creates a Dealer object
+     */
+    Dealer DEALER = new Dealer();
 }
