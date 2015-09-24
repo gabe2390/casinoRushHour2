d786f97ea76ef76e58a3052ef4f0cbbb7bfa8f97
Gabriel Humphrey <ghumphrey@Zips-MacBook-Pro-36.local>
2015-09-24 12:19:37 -0400

    GameState enum

---
+++ src/main/java/io/zipcoder/casinorushhour2/GameState.java
@@ -0,0 +1,9 @@
+package io.zipcoder.casinorushhour2;
+
+/**
+ * Created by rsparks 9/23/15.
+ * GameState enum defines the beginning and end of game loop in each game
+ */
+public enum GameState {
+    RUNNING, NOTRUNNING
+}
