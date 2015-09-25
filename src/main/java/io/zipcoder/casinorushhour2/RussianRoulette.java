19dea0bdf9b32cbaf3f369a242ced409eb01a811
gabe2390 <gabe2390@gmail.com>
2015-09-25 10:52:29 -0400

    Stuff

--- src/main/java/io/zipcoder/casinorushhour2/RussianRoulette.java
+++ src/main/java/io/zipcoder/casinorushhour2/RussianRoulette.java
@@ -7,10 +7,11 @@ import java.util.Scanner;
  * This is the class for a game of RussianRoulette. Almost all of the functionality is contained in the playGame()
  * that contains the game loop and calls other method to determine other actions.
  */
-public class RussianRoulette {
+public class RussianRoulette implements Game {
     Gun gun;
     Player player;
     GameState state = GameState.RUNNING;
+    int count = 0;
 
     public RussianRoulette(Gun gun) {
         this.gun = gun;
@@ -18,35 +19,43 @@ public class RussianRoulette {
         gun.init();
     }
 
+    /**
+     * Has the flow control for the RussianRoulette Game. Uses console prompts and a Scanner to guide player through the game
+     */
     public void playGame() {
+
+        //change GameState and instantiate a Scanner for user input
         state = GameState.RUNNING;
         Scanner key = new Scanner(System.in);
-        System.out.println(player == null);
+
+
         System.out.println("Look here ya broke bastard, you owe " + (-player.getBank()) + ". So here take this gun, and shoot at yourself!");
 
         while (state == GameState.RUNNING) {
+
+            count++; //the more you play, the higher the money multiplier is
+
             gun.spinChamber();
+            waitTwoSecondsToShoot();
 
-            long currentTime = System.nanoTime();
-
-            while ((System.nanoTime() - currentTime) <= 2000000000) {
-
-            }
-
+            //Check is player lost. If so end game and loop, else add 1000 to the player's bank
             if (checkForWinner()) {
                 System.out.println("BANG!!!!!!!");
                 System.out.println("How unlucky are we? You just killed yourself. Better luck next life!");
                 state = GameState.NOTRUNNING;
             } else {
-                player.addToBank(1000);
+                player.addToBank(1000 * count);
 
+                //If the player still owes, make them play again, else ask if they want to play again for more money
                 if (player.getBank() < 0) {
                     System.out.println("Click!\nOk, you didn't die but, you still owe sucka! Until you pay what you owe, shoot it again!\n");
                     System.out.println("You still owe " + (-player.getBank()) + "\n");
                 } else {
-                    System.out.println("Click!\nLucky bastard! You're free to go. You've got " + player.getBank() + "\n\nHowever, if you want to earn a quick $1000, you can play again");
+                    System.out.println("Click!\nLucky bastard! You're free to go. You've got " + player.getBank() + "\n\n" +
+                            "However, if you want to earn a quick " + (1000 * count) + ", you can play again");
                     System.out.println("Do you want to play again? Enter 'Y' to play again or any other key to exit.");
 
+                    //If player chooses to play again, re-enter the loop, else change GameState, thus ending the game loop, and the program
                     if (key.nextLine().equalsIgnoreCase("Y")) {
                         System.out.println("Dumb-ass! Here we go again!");
                     } else {
@@ -58,17 +67,39 @@ public class RussianRoulette {
         }
     }
 
-    private boolean checkForWinner() {
+    public void changeGameState() {
+
+    }
+
+    public void exitGame() {
+
+    }
+
+    /**
+     * If the gun shoots a bullet (returns true), player loses
+     *
+     * @return boolean
+     */
+    public boolean checkForWinner() {
         return gun.shoot();
     }
 
     public static void main(String[] args) {
-        Player p= new Player("Gabe");
+        Player p = new Player("Gabe");
         System.out.println(p == null);
         System.out.println(p.getBank());
 
         RussianRoulette r = new RussianRoulette(new Gun(p));
 
         r.playGame();
+    }
+
+    /**
+     * Pause the flow of the game for 2 seconds to add to "realism"
+     */
+    public void waitTwoSecondsToShoot() {
+        long currentTime = System.nanoTime();
+        while ((System.nanoTime() - currentTime) <= 2000000000) {
+        }
     }
 }
--- src/test/java/test/zipcoder/casinorushhour2/RussianRouletteTest.java
+++ src/test/java/test/zipcoder/casinorushhour2/RussianRouletteTest.java
@@ -1,7 +1,42 @@
 package test.zipcoder.casinorushhour2;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import io.zipcoder.casinorushhour2.Gun;
+import io.zipcoder.casinorushhour2.Player;
+import io.zipcoder.casinorushhour2.RussianRoulette;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by ghumphrey on 9/25/15.
  */
 public class RussianRouletteTest {
+
+    RussianRoulette game;
+    Gun gun;
+    Player player;
+
+    /**
+     * Creating a player with a negative balance is necessary to ensure the game flows correctly
+     */
+    @Before
+    public void createPlayerWithNegativeBalance(){
+        player= new Player("Gabe");
+        System.out.println(player.getBank());
+    }
+
+    @Before
+    public void instantiateRussianRoulette(){
+        gun = new Gun(player);
+        game=new RussianRoulette(gun);
+    }
+
+    @Test
+    public void testCheckForWinner(){
+        assertTrue("If the chamber has not been spun, should return true", game.checkForWinner());
+  }
+
 }
