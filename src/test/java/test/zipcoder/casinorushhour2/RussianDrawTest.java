ac76da6da6aac7a9ec4c4f15b59b7b148d6f8e44
gabe2390 <gabe2390@gmail.com>
2015-09-28 15:08:44 -0400

    Added all files

--- src/test/java/test/zipcoder/casinorushhour2/RussianDrawTest.java
+++ src/test/java/test/zipcoder/casinorushhour2/RussianDrawTest.java
@@ -1,7 +1,42 @@
 package test.zipcoder.casinorushhour2;
 
+import io.zipcoder.casinorushhour2.Gun;
+import io.zipcoder.casinorushhour2.Player;
+import io.zipcoder.casinorushhour2.RussianDraw;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertEquals;
+
 /**
- * Created by ghumphrey on 9/28/15.
+ * Created by emaron on 9/28/15.
  */
 public class RussianDrawTest {
+
+    RussianDraw drawTest = new RussianDraw(new Gun(new Player("bob")));
+
+    /**
+     * Confirms that the counters for each gun are initialized
+     */
+    @Test
+    public void shouldReturnBoolean() {
+        assertFalse("Should return false", drawTest.checkForWinner());
+    }
+
+
+    @Test
+    public void shouldReturnABooleanResult() {
+        int countAliveOrDead = 0;
+
+        if (drawTest.checkForWinner()) {
+            countAliveOrDead = 1;
+        } else if (drawTest.checkForWinner() == false) {
+            countAliveOrDead = 2;
+        }
+
+        assertTrue("countAliveOrDead is within the range of 1 - 2 " + countAliveOrDead, 0 <= countAliveOrDead && countAliveOrDead <= 2);
+    }
 }
+
